1add90f5125ce4c8e69eff2282c62f44ae7c02bf
mochida <voogie01+github@gmail.com>
2012-02-16 11:44:33 +0900

    init

---
+++ src/jp/gr/java_conf/reimochida/regulargps/RegulargpsActivity.java
@@ -0,0 +1,13 @@
+package jp.gr.java_conf.reimochida.regulargps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegulargpsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
